79ba3beea616a2d74713037254f19959936f132c
Nicholas Bishop <nicholasbishop@gmail.com>
2020-08-28 19:51:12 -0400

    Move some more code out of server_example

--- server/src/ui.rs
+++ server/src/ui.rs
@@ -1,6 +1,8 @@
 use crate::{Error, Pool};
 use askama::Template;
+use chrono::{DateTime, Utc};
 use fehler::throws;
+use log::error;
 
 #[derive(Template)]
 #[template(path = "projects.html")]
@@ -15,6 +17,115 @@ pub async fn list_projects(pool: &Pool) -> String {
 
     let template = ProjectsTemplate {
         projects: rows.iter().map(|row| row.get(1)).collect(),
+    };
+    template.render()?
+}
+
+#[derive(Default)]
+struct JobSummary {
+    job_id: i64,
+    duration: String,
+    data: serde_json::Value,
+    runner: String,
+    state: String,
+}
+
+#[derive(Template)]
+#[template(path = "project.html")]
+struct ProjectTemplate {
+    name: String,
+    recent_jobs: Vec<JobSummary>,
+    pending_jobs: Vec<JobSummary>,
+    running_jobs: Vec<JobSummary>,
+}
+
+fn format_duration(start: DateTime<Utc>, end: DateTime<Utc>) -> String {
+    let duration = if let Ok(duration) = (end - start).to_std() {
+        // Round trip the number of seconds to clear out the subsecond
+        // fields
+        std::time::Duration::from_secs(duration.as_secs())
+    } else {
+        error!("invalid duration: start={}, end={}", start, end);
+        std::time::Duration::default()
+    };
+    humantime::format_duration(duration).to_string()
+}
+
+#[throws]
+pub async fn get_project(pool: &Pool, project_name: &str) -> String {
+    let conn = pool.get().await?;
+
+    let rows = conn
+        .query(
+            "SELECT id, data
+             FROM jobs WHERE state = 'available'
+             ORDER BY priority, created
+             LIMIT 10",
+            &[],
+        )
+        .await?;
+    let pending_jobs = rows
+        .iter()
+        .map(|row| JobSummary {
+            job_id: row.get(0),
+            data: row.get(1),
+            ..JobSummary::default()
+        })
+        .collect();
+
+    let rows = conn
+        .query(
+            "SELECT id, data, runner, started, CURRENT_TIMESTAMP
+             FROM jobs WHERE state = 'running'
+             ORDER BY priority, created
+             LIMIT 10",
+            &[],
+        )
+        .await?;
+    let running_jobs = rows
+        .iter()
+        .map(|row| {
+            let started: DateTime<Utc> = row.get(3);
+            let now: DateTime<Utc> = row.get(4);
+            JobSummary {
+                job_id: row.get(0),
+                data: row.get(1),
+                runner: row.get(2),
+                duration: format_duration(started, now),
+                ..JobSummary::default()
+            }
+        })
+        .collect();
+
+    let rows = conn
+        .query(
+            "SELECT id, data, runner, started, finished, state
+             FROM jobs WHERE state != 'available' AND state != 'running'
+             ORDER BY priority, created
+             LIMIT 10",
+            &[],
+        )
+        .await?;
+    let recent_jobs = rows
+        .iter()
+        .map(|row| {
+            let started: DateTime<Utc> = row.get(3);
+            let now: DateTime<Utc> = row.get(4);
+            JobSummary {
+                job_id: row.get(0),
+                data: row.get(1),
+                runner: row.get(2),
+                duration: format_duration(started, now),
+                state: row.get(5),
+            }
+        })
+        .collect();
+
+    let template = ProjectTemplate {
+        name: project_name.into(),
+        pending_jobs,
+        running_jobs,
+        recent_jobs,
     };
     template.render()?
 }
--- server_example/src/main.rs
+++ server_example/src/main.rs
@@ -2,7 +2,6 @@ use actix_web::body::Body;
 use actix_web::{middleware, App, HttpServer};
 use actix_web::{web, HttpResponse, Responder};
 use askama::Template;
-use chrono::{DateTime, Utc};
 use env_logger::Env;
 use fehler::throws;
 use jobclerk_server::{api, ui};
@@ -12,24 +11,6 @@ use log::error;
 #[derive(Template)]
 #[template(path = "internal_error.html")]
 struct InternalErrorTemplate {}
-
-#[derive(Default)]
-struct JobSummary {
-    job_id: i64,
-    duration: String,
-    data: serde_json::Value,
-    runner: String,
-    state: String,
-}
-
-#[derive(Template)]
-#[template(path = "project.html")]
-struct ProjectTemplate {
-    name: String,
-    recent_jobs: Vec<JobSummary>,
-    pending_jobs: Vec<JobSummary>,
-    running_jobs: Vec<JobSummary>,
-}
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -66,99 +47,14 @@ async fn list_projects(pool: web::Data<Pool>) -> impl Responder {
     HttpResponse::Ok().body(ui::list_projects(pool.get_ref()).await?)
 }
 
-fn format_duration(start: DateTime<Utc>, end: DateTime<Utc>) -> String {
-    let duration = if let Ok(duration) = (end - start).to_std() {
-        // Round trip the number of seconds to clear out the subsecond
-        // fields
-        std::time::Duration::from_secs(duration.as_secs())
-    } else {
-        error!("invalid duration: start={}, end={}", start, end);
-        std::time::Duration::default()
-    };
-    humantime::format_duration(duration).to_string()
-}
-
 #[throws]
 async fn get_project(
     pool: web::Data<Pool>,
     path: web::Path<(String,)>,
 ) -> impl Responder {
     let project_name = &path.0;
-    let conn = pool.get().await?;
-
-    let rows = conn
-        .query(
-            "SELECT id, data
-             FROM jobs WHERE state = 'available'
-             ORDER BY priority, created
-             LIMIT 10",
-            &[],
-        )
-        .await?;
-    let pending_jobs = rows
-        .iter()
-        .map(|row| JobSummary {
-            job_id: row.get(0),
-            data: row.get(1),
-            ..JobSummary::default()
-        })
-        .collect();
-
-    let rows = conn
-        .query(
-            "SELECT id, data, runner, started, CURRENT_TIMESTAMP
-             FROM jobs WHERE state = 'running'
-             ORDER BY priority, created
-             LIMIT 10",
-            &[],
-        )
-        .await?;
-    let running_jobs = rows
-        .iter()
-        .map(|row| {
-            let started: DateTime<Utc> = row.get(3);
-            let now: DateTime<Utc> = row.get(4);
-            JobSummary {
-                job_id: row.get(0),
-                data: row.get(1),
-                runner: row.get(2),
-                duration: format_duration(started, now),
-                ..JobSummary::default()
-            }
-        })
-        .collect();
-
-    let rows = conn
-        .query(
-            "SELECT id, data, runner, started, finished, state
-             FROM jobs WHERE state != 'available' AND state != 'running'
-             ORDER BY priority, created
-             LIMIT 10",
-            &[],
-        )
-        .await?;
-    let recent_jobs = rows
-        .iter()
-        .map(|row| {
-            let started: DateTime<Utc> = row.get(3);
-            let now: DateTime<Utc> = row.get(4);
-            JobSummary {
-                job_id: row.get(0),
-                data: row.get(1),
-                runner: row.get(2),
-                duration: format_duration(started, now),
-                state: row.get(5),
-            }
-        })
-        .collect();
-
-    let template = ProjectTemplate {
-        name: project_name.into(),
-        pending_jobs,
-        running_jobs,
-        recent_jobs,
-    };
-    HttpResponse::Ok().body(template.render()?)
+    HttpResponse::Ok()
+        .body(ui::get_project(pool.get_ref(), project_name).await?)
 }
 
 async fn handle_api_request(
